0bbb6d91547be37a8c0174c7c5b16ff93ef78c8a
phamanhtu1012@gmail.com <phamanhtu1012@gmail.com>
2023-10-24 21:50:54 +0700

    hoan thanh buoi 28

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -21,7 +21,10 @@ class UsersService {
       options: { expiresIn: process.env.REFRESH_TOKEN_EXPIRE_IN }
     })
   }
-
+  //ký asccess_token và refresh_token
+  private signAccessAndRefreshToken(user_id: string) {
+    return Promise.all([this.signAccessToken(user_id), this.signRefreshToken(user_id)])
+  }
   async checkEmailExist(email: string) {
     const user = await databaseService.users.findOne({ email })
     return Boolean(user)
@@ -43,15 +46,16 @@ class UsersService {
     // const accessToken = await this.signAccessToken(user_Id)
     // const refreshToken = await this.signRefreshToken(user_Id)
     //nên viết là thì sẽ giảm thời gian chờ 2 cái này tạo ra
-    const [accessToken, refreshToken] = await Promise.all([
-      this.signAccessToken(user_id),
-      this.signRefreshToken(user_id)
-    ])
+    const [accessToken, refreshToken] = await this.signAccessAndRefreshToken(user_id)
     //đây cũng chính là lý do mình chọn xử lý bất đồng bộ, thay vì chọn xử lý đồng bộ
     //Promise.all giúp nó chạy bất đồng bộ, chạy song song nhau, giảm thời gian
     return { accessToken, refreshToken }
     //ta sẽ return 2 cái này về cho client
     //thay vì return user_Id về cho client
+  }
+  async login(user_id: string) {
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
+    return { access_token, refresh_token }
   }
 }
 
