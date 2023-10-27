4cdb4aa16862b441218b09d98dd9774087921629
phamanhtu1012@gmail.com <phamanhtu1012@gmail.com>
2023-10-27 22:24:25 +0700

    da fix

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -6,7 +6,7 @@ import { ErrorWithStatus } from '~/models/Errors'
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   //err là lỗi từ các nơi khác truyền xuống, và ta đã quy ước lỗi phải là 1 object có 2 thuộc tính: status và message
   if (err instanceof ErrorWithStatus) {
-    res.status(err.status).json(omit(err, ['status']))
+    return res.status(err.status).json(omit(err, ['status']))
   }
   //nếu mà lỗi xuống được đây
   //set name,stack, message cho enumerable true
